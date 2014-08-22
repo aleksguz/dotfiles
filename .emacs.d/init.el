d66a5b2925b44398cca6ec2e4d941adc38f35fe5
aleksguz <aleksguz@users.noreply.github.com>
2014-08-22 09:24:53 +0200

    Update init.el
    addtions to org mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -68,3 +68,38 @@
 ;; powerline
 (require 'powerline)
 (powerline-default-theme)
+
+;; timestamp 1 week increment
+(defun sw-one-week-bump ()
+"increase timestamp by one week"
+       (interactive) 
+       (org-timestamp-up-day 7))
+
+(global-set-key (kbd "<f7>") 'sw-one-week-bump)
+
+;; easy call of org agenda
+(global-set-key (kbd "<f9>") 'org-agenda)
+
+;; add org link types
+(require 'org)
+
+;; link type mks change package
+(org-add-link-type "cp" 'org-view-mks-cp)
+
+(defun org-view-mks-cp (cp-id)
+  "Open mks change package in graphical view."
+  (shell-command (concat "si viewcp -g " cp-id)))
+
+;; link type mks issue
+(org-add-link-type "issue" 'org-view-mks-issue)
+
+(defun org-view-mks-issue (issue-id)
+  "Open mks issue in graphical view."
+  (shell-command (concat "im viewissue -g " issue-id)))
+
+;; displays mks file history in buffer
+(defun mks-file-history (arg)
+  (interactive "sEnter Filename: ")
+  (setq sicommand "si viewhistory --hostname=ffm-mks1 --port=7001 --project=#p=d:/mks/archives/src/ct_zt8.pj#s=BFU/2013CW35_BFU.pj --fields=revision,description,cpid,author,date ")
+  (message (concat sicommand arg))
+  (shell-command (concat sicommand arg)))
